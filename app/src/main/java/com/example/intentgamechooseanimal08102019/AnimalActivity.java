cc1aa357f62c14b63552811b72df783257ebaeb2
phatdroid94 <phatdroid94@gmail.com>
2019-12-03 21:21:33 +0700

    get time current

--- app/src/main/java/com/example/intentgamechooseanimal08102019/AnimalActivity.java
+++ app/src/main/java/com/example/intentgamechooseanimal08102019/AnimalActivity.java
@@ -2,19 +2,23 @@ package com.example.intentgamechooseanimal08102019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toast;
 
 public class AnimalActivity extends AppCompatActivity {
 
-    int mValueHinhGoc = 0;
     int mCurrentime = 0;
     Handler mHandler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animal);
+        Intent intent = getIntent();
+        int time = intent.getIntExtra("currentTime",-1);
+        countDownTime(time);
+
     }
     private void countDownTime(int time) {
         mCurrentime = time;
@@ -24,6 +28,7 @@ public class AnimalActivity extends AppCompatActivity {
         @Override
         public void run() {
             if (mCurrentime < 5) {
+                mCurrentime++;
                 mHandler.postDelayed(runnable, 1000);
             } else {
                 mHandler.removeCallbacks(runnable);

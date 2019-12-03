ccb554e4b5fbbb6fd9de59ff257444db86517b2a
phatdroid94 <phatdroid94@gmail.com>
2019-12-03 20:09:03 +0700

    init

--- app/src/main/java/com/example/intentgamechooseanimal08102019/MainActivity.java
+++ app/src/main/java/com/example/intentgamechooseanimal08102019/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.intentgamechooseanimal08102019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ImageView mImgHinhGoc,mImgHinhChon;
+    ImageButton mImgPlay;
+    FrameLayout mFrameLayout;
+    TextView mTxtTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        mImgHinhChon = findViewById(R.id.imageviewHinhChon);
+        mImgHinhGoc = findViewById(R.id.imageviewHinhGoc);
+        mImgPlay = findViewById(R.id.imagebuttonPlay);
+        mFrameLayout = findViewById(R.id.framelayout);
+        mTxtTime = findViewById(R.id.textviewThoiGian);
     }
 }

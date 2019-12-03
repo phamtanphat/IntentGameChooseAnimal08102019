0ad7e444b6fcd6e7448f51054b114cf15a74498d
phatdroid94 <phatdroid94@gmail.com>
2019-12-03 20:36:36 +0700

    random image

--- app/src/main/java/com/example/intentgamechooseanimal08102019/MainActivity.java
+++ app/src/main/java/com/example/intentgamechooseanimal08102019/MainActivity.java
@@ -3,24 +3,49 @@ package com.example.intentgamechooseanimal08102019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity {
 
     ImageView mImgHinhGoc,mImgHinhChon;
     ImageButton mImgPlay;
     FrameLayout mFrameLayout;
     TextView mTxtTime;
-
+    String[] mArrayNameImage;
+    int mValueHinhGoc = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        event();
+    }
+
+    private void event() {
+        mImgPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFrameLayout.setVisibility(View.GONE);
+                randomImage();
+            }
+        });
+    }
+
+    private void randomImage() {
+        mArrayNameImage = getResources().getStringArray(R.array.arrayAnimal);
+        Collections.shuffle(Arrays.asList(mArrayNameImage));
+        mValueHinhGoc = getResources().getIdentifier(mArrayNameImage[0],"drawable",getPackageName());
+        mImgHinhGoc.setImageResource(mValueHinhGoc);
     }
 
     private void init() {
